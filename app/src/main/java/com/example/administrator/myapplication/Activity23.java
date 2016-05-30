b14ea0098ea883157d5b8769a2ceac454aa951f4
chaohuxiayun <505281348@qq.com>
2016-05-30 18:09:27 +0800

    请问

---
+++ app/src/main/java/com/example/administrator/myapplication/Activity23.java
@@ -0,0 +1,13 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/5/30.
+ */
+public class Activity23 extends BaseActivity{
+    @Override
+    public void initContentView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity23_layout);
+    }
+}
